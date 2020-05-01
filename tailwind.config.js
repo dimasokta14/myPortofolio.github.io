b873759fd8f5b4f23ac67d30a5ce992158447de6
Dimas Arifani <dimasokta14@gmail.com>
2020-05-01 13:27:55 +0700

    init repo using tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  important:true,
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      },
+      margin: {
+        '96': '24rem',
+        '128': '32rem',
+      },
+    }
+  },
+  variants: {
+    opacity: ['responsive', 'hover']
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
